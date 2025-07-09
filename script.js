b5de9895d5bf225ad24eaf44610a92d1fe191edd
KZXSoftware <kzx@kzxsoftware.com>
2025-07-09 11:00:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿document.querySelectorAll('.qa-question').forEach(btn => {
+    btn.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const answer = this.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
